7800ec54485e8077e8eed83a09ecce4f858ba333
drozdov <drozdov.dn29@gmail.com>
2024-12-15 23:48:47 +0300

    add ecosystem.config.js

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,33 @@
+import { config } from "dotenv";
+
+config({ path: "./.env.deploy" });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPO,
+  DEPLOY_REF = "origin/master",
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: "mesto-backend",
+      script: "./dist/app.js",
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      "pre-deploy": `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      "post-deploy":
+        "cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+    },
+  },
+};
