b0aa543a2a5c23d07968d5ff132c9f66ea00895e
drozdov <drozdov.dn29@gmail.com>
2024-12-16 02:09:40 +0300

    fix url backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -130,6 +130,6 @@ class Api {
   }
 }
 // Замените на адрес вашего бэкенда
-const api = new Api("https://api.drozdovdn.students.nomorepartiesco.ru/");
+const api = new Api("https://api.drozdovdn.students.nomorepartiesco.ru");
 
 export default api;
